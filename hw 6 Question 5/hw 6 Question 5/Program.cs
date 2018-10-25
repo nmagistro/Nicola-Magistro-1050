d195a2c20c909b6eefbfc46bd1acb02065957cc0
nmagistro <43425495+nmagistro@users.noreply.github.com>
2018-10-25 00:05:31 -0400

    Add files via upload

---
+++ hw 6 Question 5/hw 6 Question 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_6_Question_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+
+            }
+        }
+    }
+}
