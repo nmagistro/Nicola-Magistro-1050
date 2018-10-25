db758aaf7305721d45c79c4a1c68b0532d8e1cc5
nmagistro <43425495+nmagistro@users.noreply.github.com>
2018-10-25 00:10:11 -0400

    Add files via upload

---
+++ lab6question3/lab6question3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isBooleanVariable;
+            isBooleanVariable = false; //Assignment
+
+            //Logical Operation
+            if (isBooleanVariable = true)
+            {
+
+            }
+        }
+    }
+}
