966de55d57f043572b14a49f19ff604d2dc92965
nmagistro <43425495+nmagistro@users.noreply.github.com>
2018-10-24 23:53:41 -0400

    Add files via upload

---
+++ labw6 question 6/labw6 question 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labw6_question_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            i = 60;
+            while (20 <= i)
+            {
+                Console.WriteLine(i);
+                i = i - 1;
+
+            }
+
+
+
+        }
+    }
+}
