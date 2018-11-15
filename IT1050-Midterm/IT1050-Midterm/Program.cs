04af027ef0a915d4e5c5804594bd8b63d27d403a
nmagistro <43425495+nmagistro@users.noreply.github.com>
2018-11-14 22:10:50 -0500

    Add files via upload

---
+++ IT1050-Midterm/IT1050-Midterm/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050_Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int i = 10;
+            Boolean keepLooping = true; while (keepLooping)
+            {
+                if (i <= 9) keepLooping = false;
+                i++;
+                Console.WriteLine(i);
+
+            }
+            */
+
+            /* int n = 2; while (n <= 128)
+             {
+                 Console.Write("[{0}]", n);
+                 n++;
+             }
+             */
+
+            /* int n;
+             int count = 0;
+             int answer = 0; n = 12; count = n; while (count >= 1) { answer = answer + count; count--; }
+             Console.WriteLine("The sum of all integers from 0-12 is " + answer);
+             */
+
+           /* Console.Write("1");
+            for (int number = 3; number <= 21; number++)
+            Console.Write(",{0}", number);
+            */
+
+
+        }
+    }
+
+}
