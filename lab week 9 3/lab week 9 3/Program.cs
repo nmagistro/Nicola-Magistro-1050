043d7161f0407e7a3472d2620596955dd761500d
nmagistro <43425495+nmagistro@users.noreply.github.com>
2018-11-21 22:35:10 -0500

    Add files via upload

---
+++ lab week 9 3/lab week 9 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_week_9_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myMath iMyMath = new myMath();
+
+
+
+            iMyMath.Multiply(12,24);
+            Console.WriteLine("The result for multiplication is: " + iMyMath.GetResult());
+
+            iMyMath.Divide(15, 30);
+            Console.WriteLine("The result of division is:" + iMyMath.GetResult());
+
+            iMyMath.Subtract(30, 20);
+            Console.WriteLine("The result of subtraction is:" + iMyMath.GetResult());
+
+            iMyMath.Add(200, 300);
+            Console.WriteLine("The result of addition is:" + iMyMath.GetResult());
+
+
+        }
+    }
+}
---
+++ lab week 9 3/lab week 9 3/myMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_week_9_3
+{
+    class myMath
+    {
+        private double result;
+        public double operand1;
+        public double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+        }
+
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+        }
+
+        public void Subtract(double operand1, double operand2)
+        {
+            result = operand1 - operand2;
+        }
+        public void Add(double operand1, double operand2)
+        {
+            result = operand1 + operand2;
+        }
+
+        public double GetResult()
+        {
+            return result;
+        }
+
+
+    }
+}
